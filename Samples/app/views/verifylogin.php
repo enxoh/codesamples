1c3c8086bbf2c98a77875a0ec8e36ff5c48ce16b
enxoh <43008242+enxoh@users.noreply.github.com>
2019-04-15 18:39:06 -0400

    Delete verifylogin.php

--- Samples/app/views/verifylogin.php
+++
@@ -1,8 +0,0 @@
-<?php
-//include '/header.php'
-?>
-<div style="padding-left: 5px">
-<h4>You must be logged in to access this page!</h4>
-<a href='home/Login'>Click here to login or register.</a>
-</div>
-
